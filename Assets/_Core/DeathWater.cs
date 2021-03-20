a275394c0ffd98ae37138d88a96fa019b886e7e7
juan sebastian palacios <elgranpalicron@gmail.com>
2021-03-19 19:10:51 -0500

    Correciones

---
+++ Assets/_Core/DeathWater.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathWater : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag.Equals("Player"))
+        {
+            GameManager.intance.resetPlayer(35);
+        }
+       
+    }
+}
