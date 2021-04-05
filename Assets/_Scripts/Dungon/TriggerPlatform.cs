1325457ba8c7044b60b7939279ea149dc1e7ad2b
Juan Camilo Sánchez <jc.sanchez12@uniandes.edu.co>
2021-04-05 15:28:52 -0500

    AddingSwichText

--- Assets/_Scripts/Dungon/TriggerPlatform.cs
+++ Assets/_Scripts/Dungon/TriggerPlatform.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TriggerPlatform : MonoBehaviour
 {
 
     public PlaftormController platformCt;
     [Header("ON/OFF VALUES")]
+    public TextMeshPro state;
     public float Velx;
     public float Velz;
 
@@ -14,14 +16,14 @@ public class TriggerPlatform : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            if(platformCt.type == PlaftormController.PlatformType.TRIGGEREXIT )
+            if (platformCt.type == PlaftormController.PlatformType.TRIGGEREXIT)
             {
-             platformCt.active = false;
-            StartCoroutine(WaitForMove(2f));
+                platformCt.active = false;
+                StartCoroutine(WaitForMove(2f));
             }
             else if (platformCt.type == PlaftormController.PlatformType.TRANSPORTPLAYER)
             {
-                if (Velx!=0) 
+                if (Velx != 0)
                 {
                     platformCt.VelX = Velx;
                 }
@@ -29,6 +31,11 @@ public class TriggerPlatform : MonoBehaviour
                 {
                     platformCt.Velz = Velz;
                 }
+            }
+            else if(state!=null)
+            {
+                platformCt.active = true;
+                state.text = "ON";  
             }
             else {platformCt.active = true;} 
         }
