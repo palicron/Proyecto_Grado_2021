40947bfd5f04a940212b4d4cc89fadac0115f022
Juan Camilo Sánchez <jc.sanchez12@uniandes.edu.co>
2021-04-09 01:47:03 -0500

    LittleFixes

--- Assets/_Scripts/Dungon/TriggerMultipleRigid.cs
+++ Assets/_Scripts/Dungon/TriggerMultipleRigid.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TriggerMultipleRigid : MonoBehaviour
 {
     public PlaftormController[] platforms;
+    public TextMeshPro anuncio;
+    public string textoAnuncio;
+    public float sizeText;
     public float speed;
 
     private void OnTriggerEnter(Collider other)
@@ -18,6 +22,11 @@ public class TriggerMultipleRigid : MonoBehaviour
                     pt.platformSpeed = speed;
                     pt.active = true;
                 }
+            }
+            if (anuncio!=null) 
+            {
+                anuncio.fontSize = sizeText;
+                anuncio.text = textoAnuncio;
             }
         }
     }
