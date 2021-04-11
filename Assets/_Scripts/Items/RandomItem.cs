d72b3fbf93c63ec2e6dcf385d3dab9f33af30ad3
Julian <js.amezquita@uniandes.edu.co>
2021-04-11 18:08:06 -0500

    Paper level items added
    Added new items and random for rare and common items

--- Assets/_Scripts/Items/RandomItem.cs
+++ Assets/_Scripts/Items/RandomItem.cs
@@ -6,10 +6,17 @@ public class RandomItem : MonoBehaviour
 {
     [SerializeField]
     GameObject[] items;
+    [SerializeField]
+    bool isFloating;
     // Start is called before the first frame update
     void Start()
     {
         GameObject item = Instantiate(items[Random.Range(0, items.Length)], transform.position, Quaternion.identity);
+        if(isFloating)
+        {
+            Destroy(item.GetComponent<Rigidbody>());
+            item.GetComponent<BoxCollider>().enabled = false;
+        }
         item.transform.parent = gameObject.transform;
     }
 }
