ed93cbc61616c4305395814c206210a8bff759ca
Juan Camilo Sánchez <30872909+JuanCamiloSanchez95@users.noreply.github.com>
2021-05-01 11:59:06 -0500

    Room2Corrections

--- Assets/_Scripts/Dungon/Puzzles/FinalPuzzle/PuzzleCategoria/MetalQuestionTrigger.cs
+++ Assets/_Scripts/Dungon/Puzzles/FinalPuzzle/PuzzleCategoria/MetalQuestionTrigger.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MetalQuestionTrigger : MonoBehaviour
 {
     [Header("Dependences")]
     public MetalRoomManager manager;
     public PlaftormController plat;
+    public TextMeshPro stateText;
     [Header("Trigger Characteristics")]
     public bool activated;
 
+    private void Start()
+    {
+        stateText.text = "Activador \n en espera";
+    }
 
 
     private void OnTriggerEnter(Collider other)
@@ -18,6 +24,7 @@ public class MetalQuestionTrigger : MonoBehaviour
         {
             if (!activated)
             {
+                stateText.text = "Activador \n generado";
                 if (manager.QaA.Count != 0)
                 {
                     manager.generateQuestion();
